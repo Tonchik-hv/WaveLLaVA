bc67ae6fd3adb1d58830d3fb95230a71c7550fb0
Tonchik-hv <73605475+Tonchik-hv@users.noreply.github.com>
2024-08-30 00:17:54 +0500

    Add files via upload

---
+++ version1.2/models_level.py
@@ -0,0 +1,83 @@
+import math
+import torch
+import torch.nn as nn
+from PIL import Image
+
+def initialize_special_embs(cfg):
+    special_embs = {}
+    special_embs['USER'] = torch.normal(torch.zeros(cfg.emb_dim), torch.ones(cfg.emb_dim) / cfg.emb_dim**0.5).to(dtype=torch.bfloat16)
+    special_embs['BOT'] = torch.normal(torch.zeros(cfg.emb_dim), torch.ones(cfg.emb_dim) / cfg.emb_dim**0.5).to(dtype=torch.bfloat16)
+    special_embs['SOI'] = torch.normal(torch.zeros(cfg.emb_dim), torch.ones(cfg.emb_dim) / cfg.emb_dim**0.5).to(dtype=torch.bfloat16)
+    special_embs['EOI'] = torch.normal(torch.zeros(cfg.emb_dim), torch.ones(cfg.emb_dim) / cfg.emb_dim**0.5).to(dtype=torch.bfloat16)
+
+    special_embs['SOI'].requires_grad_()
+    special_embs['EOI'].requires_grad_()
+    special_embs['USER'].requires_grad_()
+    special_embs['BOT'].requires_grad_()
+    return special_embs
+
+
+
+
+class ConvQ(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.Q1 = nn.Conv2d(in_ch, in_ch, kernel_size = 3, stride = 1, padding = 1)
+        self.Q2 = nn.Conv2d(in_ch, 125, kernel_size = 3, stride = 1, padding = 1)
+        self.Q3 = nn.Conv2d(125, out_ch, kernel_size = 3, stride = 1, padding = 1)
+        self.MXPL = nn.MaxPool2d(kernel_size=2)
+        self.norm = nn.LayerNorm(out_ch)
+
+    def forward(self, x):
+        # B, S, D
+        B, S, D = x.shape
+    
+        size = int(math.sqrt(x.shape[1]))
+
+        x = x.view(B, size, size, D).permute(0, 3, 1, 2)
+        
+        out1 = self.Q1(x)
+        out2 = self.MXPL(out1)
+        out3 = self.Q2(out2)
+        out4 = self.MXPL(out3)
+        out5 = self.Q3(out4)
+        out6 = self.MXPL(out5)
+     
+        out = self.norm(out6.flatten(2).permute(0, 2, 1))
+
+        return out
+
+
+class VisualToGPTMapping(nn.Module):
+    def __init__(self, visual_emb_dim, gpt_emb_dim):
+        super(VisualToGPTMapping, self).__init__()
+        
+        # self.Q = nn.Sequential(
+                    # nn.Linear(visual_emb_dim, visual_emb_dim), nn.GELU())
+        
+        self.Q = ConvQ(visual_emb_dim, 256)
+
+        self.pos = nn.Parameter(torch.randn(size = (1, 256, 256)))
+        
+        # transformer_layer = nn.TransformerEncoderLayer(d_model=visual_emb_dim, nhead=8, batch_first=True, norm_first=False)
+        self.mlp = nn.Sequential(
+                    nn.Linear(256, gpt_emb_dim),
+                    nn.GELU(),
+                    nn.Linear(gpt_emb_dim, gpt_emb_dim))
+        # self.transformer = nn.TransformerEncoder(transformer_layer, num_layers=2)
+        
+        # self.linear = nn.Linear(visual_emb_dim, gpt_emb_dim)
+        
+    
+    def forward(self, visual_embs0):
+        B = visual_embs0.shape[0]
+        E = self.Q(visual_embs0) + self.pos.expand(B, -1, -1)
+        out = self.mlp(E)
+        # out = self.linear(out)
+        
+        return out
+
+
+
+
+
